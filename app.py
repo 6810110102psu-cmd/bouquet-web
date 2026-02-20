afbb518d8ca00ad1348ce08362f20330f532c838
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-20 16:35:41 +0000

    implement user registration with database

--- app.py
+++ app.py
@@ -24,8 +24,18 @@ def about():
 def login():
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect("/login")
+
     return render_template("register.html")
 
 if __name__ == "__main__":

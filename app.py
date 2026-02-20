fcbb011657e8bd4f3b2103db938a0c10430d57ce
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-20 16:33:42 +0000

    add user model

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+# สร้างฐานข้อมูลครั้งแรก
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 @app.route("/")
 def home():

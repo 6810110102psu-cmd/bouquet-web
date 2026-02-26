0b9bb9010f198e57c860a4fa146a4ca0901e2540
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-26 16:37:46 +0000

    แก้ไขโค้ดส่วนพรีวิวหน้าเว็บ

--- app.py
+++ app.py
@@ -33,18 +33,7 @@ def home():
 
 @app.route("/flowers")
 def flowers():
-    flowers = [
-        {"id": 1, "name": "Lily", "image": "images/flowers/1_Lily.png"},
-        {"id": 2, "name": "Daisy", "image": "images/flowers/2_Daisy.png"},
-        {"id": 3, "name": "Dandelion", "image": "images/flowers/3_Dandelion.png"},
-        {"id": 4, "name": "Sunflower", "image": "images/flowers/4_Sunflowers.png"},
-        {"id": 5, "name": "Bluedaze", "image": "images/flowers/5_Blue_daze.png"},
-        {"id": 6, "name": "Lavendula spica", "image": "images/flowers/6_Lavandula_spica.png"},
-        {"id": 7, "name": "Peony", "image": "images/flowers/7_Peony_flower.png"},
-        {"id": 8, "name": "Red Holly", "image": "images/flowers/8_Red_Holly.png"},
-        {"id": 9, "name": "Calluna", "image": "images/flowers/9_Calluna.png"},
-        {"id": 10, "name": "Tulip", "image": "images/flowers/10_Tulip.png"},
-    ]
+    flowers = Flower.query.all()
     return render_template("flowers.html", flowers=flowers)
 
 @app.route("/add_to_bouquet/<int:flower_id>")
@@ -55,13 +44,16 @@ def add_to_bouquet(flower_id):
     session["bouquet"].append(flower_id)
     session.modified = True
 
-    flash("เพิ่มดอกไม้ลงในช่อแล้ว 🌸", "success")
+    flash("เพิ่มดอกไม้ลงในช่อแล้ว", "success")
     return redirect("/flowers")
-
 @app.route("/preview")
 def preview():
     flower_ids = session.get("bouquet", [])
-    selected_flowers = Flower.query.filter(Flower.id.in_(flower_ids)).all()
+
+    selected_flowers = Flower.query.filter(
+        Flower.id.in_(flower_ids)
+    ).all()
+
     return render_template("preview.html", flowers=selected_flowers)
 
 @app.route("/about")
@@ -107,4 +99,6 @@ def logout():
     return redirect("/")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)

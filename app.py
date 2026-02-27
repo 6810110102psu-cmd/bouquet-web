5bd98667bcaa45db70538be2a752ead8b449bb0c
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-27 16:34:17 +0000

    connect preview to final bouquet page

--- app.py
+++ app.py
@@ -141,6 +141,10 @@ def remove_flower(flower_id):
 
     return redirect("/preview")
 
+@app.route("/final")
+def final():
+    return render_template("final.html")
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()

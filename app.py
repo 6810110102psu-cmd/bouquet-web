27e50356d9910ee51943d475b626a4f88fc5894f
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-28 13:13:19 +0000

    add new file for new data flower's name

--- app.py
+++ app.py
@@ -168,3 +168,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+    
+    
+    
+    
+    
---
+++ seed_flowers.py
@@ -0,0 +1,29 @@
+from app import app, db, Flower
+
+with app.app_context():
+    flowers = [
+        Flower(name="Lily of the Valley", image="images/flowers/1.png"),
+        Flower(name="White Rose", image="images/flowers/2.png"),
+        Flower(name="Dusty Purple Rose", image="images/flowers/3.png"),
+        Flower(name="Waxflower", image="images/flowers/4.png"),
+        Flower(name="Carnation", image="images/flowers/5.png"),
+        Flower(name="Gerbera", image="images/flowers/6.png"),
+        Flower(name="Hydrangea", image="images/flowers/7.png"),
+        Flower(name="Tulip", image="images/flowers/8.png"),
+        Flower(name="Hyacinth", image="images/flowers/9.png"),
+        Flower(name="Calla Lily", image="images/flowers/10.png"),
+        Flower(name="Phalaenopsis Orchid", image="images/flowers/11.png"),
+        Flower(name="Marigold", image="images/flowers/12.png"),
+        Flower(name="Daisy",image="images/flowers/13.png"),
+        Flower(name="Anthurium", image="images/flowers/14.png"),
+        Flower(name="Sunflower", image="images/flowers/15.png"),
+        Flower(name="Aster", image="images/flowers/16.png"),
+        Flower(name="Gypsophila", image="images/flowers/17.png"),
+        Flower(name="Lily", image="images/flowers/18.png"),
+        Flower(name="Eucalyptus", image="images/flowers/19.png"),
+    ]
+
+    db.session.add_all(flowers)
+    db.session.commit()
+    
+    print("Seed flowers completed!")
